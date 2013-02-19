dc27465be826c2fd2410ac40e07578bde9dec8a7
Stef Van Eepoel <svaneepoel@gmail.com>
2013-02-19 22:03:06 +0100

    About us pagina met contactformulier --> nog geen validatie

---
+++ application/views/about.php
@@ -0,0 +1,94 @@
+<div class="container">
+
+	<div class="well" style="background:white; margin-top:0px;">
+
+		<div style="text-align:center;">
+			<div class='input-append' style="height:50px;">
+				<div class="page-header">
+					<h1>PXL-Exchange <small>About us</small></h1>
+				</div>
+
+			</div>
+		</div>
+		<br/>
+
+		<p>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+		</p>
+
+		<blockquote>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</blockquote>
+		<blockquote>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+			</p>
+		</blockquote>
+		<form class="form-horizontal" action="view" method="post">
+			<fieldset>
+				<div id="legend" class="">
+					<legend class="">
+						Contact us!
+					</legend>
+				</div>
+				<div class="control-group">
+
+					<!-- Text input-->
+					<label class="control-label" for="input01">Volledige naam</label>
+					<div class="controls">
+						<input name="naam" type="text" placeholder="Jan Janssen" class="input-xlarge span5">
+						<p class="help-block"></p>
+					</div>
+				</div>
+
+				<div class="control-group">
+
+					<!-- Text input-->
+					<label class="control-label" for="input01">Email</label>
+					<div class="controls">
+						<input name="emailadres" type="text" placeholder="me@example.com" class="input-xlarge span5">
+						<p class="help-block">
+							We would like to contact you back!
+						</p>
+					</div>
+				</div>
+
+				<div class="control-group">
+
+					<!-- Textarea -->
+					<label class="control-label">Message</label>
+					<div class="controls">
+						<div class="textarea">
+							<textarea name="bericht" type="" class="span5" style="resize: vertical;"> </textarea>
+						</div>
+					</div>
+				</div>
+
+				<div class="control-group">
+					<label class="control-label"></label>
+					<div class="controls">
+						<!-- Multiple Checkboxes -->
+						<label class="checkbox">
+							<input type="checkbox" value="Send me a copy!">
+							Send me a copy! </label>
+					</div>
+
+				</div>
+				<div class="control-group">
+					<label class="control-label"></label>
+
+					<!-- Button -->
+					<div class="controls">
+						<button name="submit" type="submit" class="btn btn-success">
+							Send
+						</button>
+					</div>
+				</div>
+
+			</fieldset>
+		</form>
+	</div>
+</div>
+

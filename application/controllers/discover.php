8803fb4234ed53ad6c398e70a0e8aa90642d9e5d
Stef Van Eepoel <svaneepoel@gmail.com>
2013-02-19 19:35:17 +0100

    Discover pagina - Zoeken op plaatsnaam - Toevoegen van markers uit de database

--- application/controllers/discover.php
+++ application/controllers/discover.php
@@ -2,27 +2,73 @@
 
 class Discover extends CI_Controller {
 
+	/*public function view($page = 'home') {
+
+	 $this -> load -> library('googlemaps');
+
+	 $config['center'] = '37.4419, -122.1419';
+	 $config['zoom'] = 'auto';
+	 $config['cluster'] = TRUE;
+
+	 $this -> googlemaps -> initialize($config);
+
+	 $marker = array();
+	 $marker['position'] = '37.429, -122.1419';
+	 $marker['title'] = "Dit is een test";
+	 $this -> googlemaps -> add_marker($marker);
+
+	 $data['map'] = $this -> googlemaps -> create_map();
+
+	 $this -> load -> view('header');
+	 $this -> load -> view('discover', $data);
+	 $this -> load -> view('footer');
+	 }*/
+
 	public function view($page = 'home') {
 
 		$this -> load -> library('googlemaps');
 
-		$config['center'] = '37.4419, -122.1419';
+		$config['center'] = $this -> input -> post("test"); ;
 		$config['zoom'] = 'auto';
 		$config['cluster'] = TRUE;
+		$config['places'] = TRUE;
+		$config['placesAutocompleteInputID'] = 'myPlaceTextBox';
+		$config['placesAutocompleteBoundsMap'] = TRUE;
 		$this -> googlemaps -> initialize($config);
-
-		$marker = array();
-		$marker['position'] = '37.429, -122.1419';
-		$marker['title'] = "Dit is een test";
-		$this -> googlemaps -> add_marker($marker);
 
 		
 
 		$data['map'] = $this -> googlemaps -> create_map();
-
 		$this -> load -> view('header');
 		$this -> load -> view('discover', $data);
 		$this -> load -> view('footer');
+
 	}
+	
+	public function addmarkers($page = 'home') {
+		$this -> load -> library('googlemaps');
+		$config['zoom'] = '3';
+		$config['cluster'] = TRUE;
+		$config['center'] = "belgium";
+		$this -> googlemaps -> initialize($config);
+		
+		// BEGIN MARKERS - Alle markers uit database halen en toevoegen aan de kaart
+		$query = $this -> db -> query('SELECT latitude, longitude, information FROM `points`');
+		foreach ($query->result_array() as $row) {
+			$latitude = $row['latitude'];
+			$longitude = $row['longitude'];
+			$marker = array();
+			$marker['position'] = $latitude . " , " . $longitude;
+			$marker['draggable'] = TRUE;
+			$marker['animation'] = 'DROP';
+			$marker['infowindow_content'] = $row['information'];
+			$this -> googlemaps -> add_marker($marker);
+		}
+		// END MARKERS
+		$data['map'] = $this -> googlemaps -> create_map();
+		$this -> load -> view('header');
+		$this -> load -> view('discover', $data);
+		$this -> load -> view('footer');
+		}
 
 }
--- application/views/discover.php
+++ application/views/discover.php
@@ -4,13 +4,17 @@
 	}
 </style>
 <div class="zoeken">
-	<form class="form-search">
-		<input type="text" class="input-medium search-query">
+	<form class="form-search" action="view" method="post">
+		<input name="test" id="myPlaceTextBox" type="text" class="input-medium search-query">
 		<button type="submit" class="btn">
 			Search
 		</button>
 	</form>
 </div>
+<script type="text/javascript">
+		var centreGot = false;
+	</script>
 <?php echo $map['js']; ?>
 <?php echo $map['html']; ?>
 <div class="container">
+

232a6764e9c57ecd655860ab78954ee5144bd571
Stef Van Eepoel <svaneepoel@gmail.com>
2013-02-19 19:59:51 +0100

    Layout login heel klein beetje aangepast :p

--- application/views/login.php
+++ application/views/login.php
@@ -1,32 +1,36 @@
-<div id="container">
 
 
-<h1> Login</h1>
+<div class="container">
 
-	<?php
-	
-	echo form_open('login/login_validation');
-	
-	echo validation_errors();
-	
-	echo "<p>Email: ";
-	echo form_input('email', $this->input->post('email'));
-	echo "</p>";
-	
-	echo "<p>Password ";
-	echo form_password('password');
-	echo "</p>";
+	<div class="well" style="background:white; margin-top:0px;">
+<h1>Login</h1>
+		<div style="text-align:center;">
+			<div class='input-append' style="height:50px;">
+				<?php
 
-	echo "<p>";
-	echo form_submit('login_submit', 'Login');
-	echo "</p>";
-	
-	
-	
-	echo form_close();
-	
-	?>
-	
-	<a href='<?php echo base_url()."login/signup"; ?>'>Sign Up</a>
+				echo form_open('login/login_validation');
 
+				echo validation_errors();
+
+				echo "<p>Email: ";
+				echo form_input('email', $this -> input -> post('email'));
+				echo "</p>";
+
+				echo "<p>Password ";
+				echo form_password('password');
+				echo "</p>";
+
+				echo "<p>";
+				echo form_submit('login_submit', 'Login');
+				echo "</p>";
+
+				echo form_close();
+			?>
+
+				<a href='<?php echo base_url() . "login/signup"; ?>'>Sign Up</a>
+			</div>
+		</div>
+
+	</div>
 </div>
+<div class="container">

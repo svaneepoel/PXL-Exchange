c4c0e42540d59a447d67d430f4bbd2ad2f049f69
Stef Van Eepoel <svaneepoel@gmail.com>
2013-02-20 20:01:43 +0100

    - Random name frontpage - verwijderen van bestanden van de tutorial

--- application/controllers/pages.php
+++ application/controllers/pages.php
@@ -18,36 +18,52 @@ class Pages extends CI_Controller {
 		$marker['icon'] = 'http://yovogan.free.fr/memes/problem.jpg';
 		$this -> googlemaps -> add_marker($marker);
 
-		
-
 		$data['map'] = $this -> googlemaps -> create_map();
 
 		$this -> load -> view('header');
 		$this -> load -> view('maps', $data);
 		$this -> load -> view('footer');
 	}
-	
-	public function home(){
-		
+
+	public function home() {
+		// BEGIN Random personen selecteren
+
+		$query2 = $this -> db -> query("SELECT vnaam FROM `users` ORDER BY RAND() LIMIT 0,4;");
+		$row = $query2 -> row();
+		$vnaam = $row -> vnaam;
+		$data['person1'] = $vnaam;
+
+
+
+		/*$count = $this -> db -> count_all('users');
+		$random = rand(7, 15);
+		$query = $this -> db -> query('SELECT vnaam FROM `users` WHERE id =' . $random);*/
+
+		//echo $items[array_rand($items)];
+
+		//$row = $query -> row();
+		//echo $row -> vnaam;
+
+		//END Random personen selecteren
 		$this -> load -> library('googlemaps');
 
 		$config['center'] = '37.4419, -122.1419';
 		$config['map_height'] = '200px';
-		$this -> googlemaps -> initialize($config);		
+		$this -> googlemaps -> initialize($config);
 
 		$data['map'] = $this -> googlemaps -> create_map();
-		$this->load->view('header', $data);
-		$this->load->view('home');
-		$this->load->view('footer');
-		
+		$this -> load -> view('header', $data);
+		$this -> load -> view('home');
+		$this -> load -> view('footer');
+
 	}
-	
-	public function error_404(){
+
+	public function error_404() {
 		$html = '<div style="text-align:center;"><h1 style="font-size:36pt; font-weight:normal;">Uh oh! It\'s empty here</h1><br/><p class="lead">The requested page could not be found.</p></div>';
-		
-		$this->load->view('header');
-		$this->output->append_output($html);
-		$this->load->view('footer');
+
+		$this -> load -> view('header');
+		$this -> output -> append_output($html);
+		$this -> load -> view('footer');
 	}
 
 }
--- application/views/home.php
+++ application/views/home.php
@@ -1,14 +1,42 @@
-
-		
-			<div style="text-align:center;">
-		<div class='input-append' style="height:50px;">
-			<input placeholder="Zoeken" type="text" style="background-color:#f9f9f9; height:40px; width: 450px; font-size:16pt;" />
-			<button class='btn add-on' style="width:50px;">
-				<i class="icon-search"></i>
-			</button>
-		</div>
-	</div>	
-		
+<div style="text-align:center;">
+	<div class='input-append' style="height:50px;">
+		<input placeholder="Zoeken" type="text" style="background-color:#f9f9f9; height:40px; width: 450px; font-size:16pt;" />
+		<button class='btn add-on' style="width:50px;">
+			<i class="icon-search"></i>
+		</button>
 	</div>
+</div>
+</div>
+<div class="container" style="text-align:center">
+	<div class="well clearfix" style="background:white; margin-top:0px;">
+		<div class="container">
+			<h2>Most active persons on our site!</h2><hr>
+			<div class="row">
+				<div class="span12">
+				<div class="span3 offset1">
+					<div class="well">
+						<img src="http://placehold.it/150x150">
+						<h4><?php echo $person1; ?></h4>
+						<a href="#" class='btn btn-small btn-success'>More info</a>
+					</div>
+				</div>
+				<div class="span3">
+					<div class="well">
+						<img src="http://placehold.it/150x150">
+						<h4>Person 1</h4>
+						<a href="#" class='btn btn-small btn-success'>More info</a>
 
+					</div>
+				</div>
+				<div class="span3">
+					<div class="well">
+						<img src="http://placehold.it/150x150">
+						<h4>Person 1</h4>
+						<a href="#" class='btn btn-small btn-success'>More info</a>
+
+					</div>
+				</div></div>
+			</div>
+		</div>
+	</div>
 
